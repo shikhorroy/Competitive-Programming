36d9e74c3747f4463de3ab04c34c65b7dccc2948
Roy <shikhorroy.cse12@gmail.com>
2018-03-26 11:26:20 +0600

    primary files...

---
+++ utils/utility/Pair.java
@@ -0,0 +1,45 @@
+package utils.utility;
+
+public class Pair<K, V> {
+
+    private K key;
+
+    private V value;
+
+    public Pair() {
+    }
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Pair of(K key, V value) {
+        return new Pair(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (key != null ? key.hashCode() : 0);
+        hash = 31 * hash + (value != null ? value.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof Pair) {
+            Pair pair = (Pair) o;
+            if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
+            if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
+            return true;
+        }
+        return false;
+    }
+}
