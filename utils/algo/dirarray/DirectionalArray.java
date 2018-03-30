e92092c03cdf050419077f3238585b0200737fbb
Roy <shikhorroy.cse12@gmail.com>
2018-03-30 11:30:26 +0600

    CHelper removing the bracket of two dimensional array declaration, e.g. Integer[][] <-- "[][]" these brackets are removed. So dirrArray changed to Map.

--- utils/algo/dirarray/DirectionalArray.java
+++ utils/algo/dirarray/DirectionalArray.java
@@ -1,20 +1,23 @@
 package utils.algo.dirarray;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DirectionalArray {
 
     public enum Direction {
-        FOUR,   //-> index: 0
-        EIGHT,  //-> index: 1
-        KNIGHT  //-> index: 2
+        FOUR, EIGHT, KNIGHT
     }
 
-    private static final Integer dirArray[][][] = {
-            {{1, 0, -1, 0}, {0, 1, 0, -1}}, // 4 - directions
-            {{1, 1, 0, -1, -1, -1, 0, 1}, {0, 1, 1, 1, 0, -1, -1, -1}}, // 8 - directions
-            {{2, 1, -1, -2, -2, -1, 1, 2}, {1, 2, 2, 1, -1, -2, -2, -1}} // Knight - directions
-    };
+    private static final Map<Direction, Integer[][]> dirrArray = new HashMap<>();
 
-    public static Integer[][] getDirectinalArray(Direction dir) {
-        return dirArray[dir.ordinal()];
+    static {
+        dirrArray.put(Direction.FOUR, new Integer[][]{{1, 0, -1, 0}, {0, 1, 0, -1}}); // 4 - directions
+        dirrArray.put(Direction.EIGHT, new Integer[][]{{1, 1, 0, -1, -1, -1, 0, 1}, {0, 1, 1, 1, 0, -1, -1, -1}}); // 8 - directions
+        dirrArray.put(Direction.KNIGHT, new Integer[][]{{2, 1, -1, -2, -2, -1, 1, 2}, {1, 2, 2, 1, -1, -2, -2, -1}}); // KNIGHT - directions
+    }
+
+    public static Integer[][] getDirectionalArray(Direction dir) {
+        return dirrArray.get(dir);
     }
 }
