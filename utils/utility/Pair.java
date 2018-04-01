d291063a43f215900065417652404ef5a8916d55
Roy <shikhorroy.cse12@gmail.com>
2018-04-01 21:22:02 +0600

    private key-value changed to public...

--- utils/utility/Pair.java
+++ utils/utility/Pair.java
@@ -2,17 +2,9 @@ package utils.utility;
 
 public class Pair<K, V> {
 
-    private K key;
+    public K key;
 
-    public K getKey() {
-        return key;
-    }
-
-    private V value;
-
-    public V getValue() {
-        return value;
-    }
+    public V value;
 
     public Pair(K key, V value) {
         this.key = key;
