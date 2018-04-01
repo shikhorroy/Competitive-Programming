00e34a80d30c1a936db1d78c375721a86137f346
Roy <shikhorroy.cse12@gmail.com>
2018-04-01 21:27:23 +0600

    proper naming...methods and variables.

--- utils/algo/dirarray/DirectionalArray.java
+++ utils/algo/dirarray/DirectionalArray.java
@@ -9,15 +9,15 @@ public class DirectionalArray {
         FOUR, EIGHT, KNIGHT
     }
 
-    private static final Map<Direction, Integer[][]> dirrArray = new HashMap<>();
+    private static final Map<Direction, Integer[][]> directionsArray = new HashMap<>();
 
     static {
-        dirrArray.put(Direction.FOUR, new Integer[][]{{1, 0, -1, 0}, {0, 1, 0, -1}}); // 4 - directions
-        dirrArray.put(Direction.EIGHT, new Integer[][]{{1, 1, 0, -1, -1, -1, 0, 1}, {0, 1, 1, 1, 0, -1, -1, -1}}); // 8 - directions
-        dirrArray.put(Direction.KNIGHT, new Integer[][]{{2, 1, -1, -2, -2, -1, 1, 2}, {1, 2, 2, 1, -1, -2, -2, -1}}); // KNIGHT - directions
+        directionsArray.put(Direction.FOUR, new Integer[][]{{1, 0, -1, 0}, {0, 1, 0, -1}}); // 4 - directions
+        directionsArray.put(Direction.EIGHT, new Integer[][]{{1, 1, 0, -1, -1, -1, 0, 1}, {0, 1, 1, 1, 0, -1, -1, -1}}); // 8 - directions
+        directionsArray.put(Direction.KNIGHT, new Integer[][]{{2, 1, -1, -2, -2, -1, 1, 2}, {1, 2, 2, 1, -1, -2, -2, -1}}); // KNIGHT - directions
     }
 
-    public static Integer[][] getDirectionalArray(Direction dir) {
-        return dirrArray.get(dir);
+    public static Integer[][] getDirections(Direction direction) {
+        return directionsArray.get(direction);
     }
 }
