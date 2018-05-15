67aca60d9097a01a87eb8687da8abd88c87d5b42
Roy <shikhorroy.cse12@gmail.com>
2018-05-15 22:36:39 +0600

    Return Type always BigInteger.

--- utils/algo/math/GCD.java
+++ utils/algo/math/GCD.java
@@ -1,12 +1,13 @@
 package utils.algo.math;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class GCD {
-    public static <T> T of(T a, T b) {
+    public static <T> BigInteger of(T a, T b) {
         BigInteger A = new BigInteger(a.toString());
         BigInteger B = new BigInteger(b.toString());
 
-        return (T) A.gcd(B);
+        return A.gcd(B);
     }
 }
